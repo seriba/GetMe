5d00f0d80ebfc403f2299b1152ae6e927cdfd820
Serigne BA <seriba@Mac-mini-de-Julien-2.local>
2015-09-30 10:18:21 +0200

    Initial Commit

---
+++ GetMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetMe
+//
+//  Created by Serigne BA on 30/09/2015.
+//  Copyright (c) 2015 4Djolof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
